b8772ff142690bad7b65ef2faec65071dd20896b
Fin Yang <Fin.Yang@monash.edu>
2022-05-12 15:26:46 +1000

    Rm DTB3 and vignette_data

--- R/data.R
+++ R/data.R
@@ -23,19 +23,3 @@
 #' @source \url{https://wrds-web.wharton.upenn.edu/}
 "USbonds"
 
-
-#' Daily interest rates from 4/1/1954 to 28/12/2018
-#' 
-#' A dataset containing the daily 3 Month US Treasury Bill rate
-#' 
-#' @format A data frame with 16955 rows and 2 variables:
-#' \describe{
-#' \item{date}{vector of dates}
-#' \item{rate}{daily annualised interest rate in percentages}
-#' }
-# "DTB3"
-
-#' Results of the calculation in the vignette
-#'
-#' @format A list required to produce sample figures and table
-# "vignette_data"
